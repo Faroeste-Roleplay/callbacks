6be5aefe81cb9856d664670cff004e4b6cdc278c
klandestino7 <klandestino6@hotmail.com>
2025-03-26 12:18:46 -0300

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'Standalone callback system by PiterMcFlebor'
+version '1.0'
+
+file 'import.lua'
+
+client_scripts {
+    'client/misc.lua'
+}
+server_scripts {
+    'server/misc.lua'
+}
+
+lua54 'yes'
+
+
